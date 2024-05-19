e4705c7711baf1056e16b7fc9143713ebebcb017
TatianaDan80 <162554626+TatianaDan80@users.noreply.github.com>
2024-05-19 13:54:45 +0200

    finish

--- js/task-2.js
+++ js/task-2.js
@@ -2,8 +2,9 @@ function getShippingMessage(country, price, deliveryFee) {
     const totalPrice = price + deliveryFee;
     return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
-console.log(getShippingMessage("USA", 100, 20));
-console.log(getShippingMessage("UK", 200, 30));
-console.log(getShippingMessage("Canada", 50, 15));
+console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
+console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+
 
 
