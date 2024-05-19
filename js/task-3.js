2ba4dbf331a502f433cc525eb1a673d4e286d109
TatianaDan80 <162554626+TatianaDan80@users.noreply.github.com>
2024-05-19 11:16:24 +0200

    const

--- js/task-3.js
+++ js/task-3.js
@@ -1,20 +1,18 @@
 function getElementWidth(content, padding, border) {
-    var contentWidth = parseFloat(content);
-    var paddingWidth = parseFloat(padding);
-    var borderWidth = parseFloat(border);
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
     
-    var totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+    const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
 
     return totalWidth;
 }
 
 
 console.log(getElementWidth("100px", "10px", "2px")); //  124
-console.log(getElementWidth("200px", "15px", "3px")); //  246
+console.log(getElementWidth("200px", "15px", "3px")); //  236
 console.log(getElementWidth("50px", "5px", "1px"));   //  62 
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
 
 
